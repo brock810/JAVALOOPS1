847a1e863f502a5beb190c7412205d527608cedc
brock810 <130688727+brock810@users.noreply.github.com>
2023-06-05 12:51:42 -0500

    another file

--- app.js
+++ app.js
@@ -76,13 +76,13 @@ x++;
 let numberToFind = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
 let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
 
-for ( let i = 1; i <= n; i++) {
-    if (i == numberToFind) {
-    console.log('Found ${numberToFind}!');
-    break;
+for (let i = 1; i <= n; i++) {
+    if (i === numberToFind) {
+        console.log(`Found ${numberToFind}!`);
+        break;
     }
-    if(i == n) {
-        console.log('Did not find $ {numberToFind} within 1-${n}..');
+    if (i === n) {
+        console.log(`Did not find ${numberToFind} within 1-${n}.`);
     }
 }
 
